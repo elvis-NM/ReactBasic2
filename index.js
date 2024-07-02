6a13ee0558d9ba4fcc789f3f036cd7b3ec4bc8ed
Elvis Makia <elvis_makia@yahoo.com>
2024-07-02 16:24:17 -0400

    2nd  commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 
 
-ReactDOM.render(
-<div>
-    <h1>React Course</h1>
-    <p>We will learn react in this course</p>
-</div>, 
-document.getElementById("root")
-);
+// ReactDOM.render(
+// <div>
+//     <h1>React Course</h1>
+//     <p>We will learn react in this course</p>
+// </div>, 
+// document.getElementById("root")
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<div>
